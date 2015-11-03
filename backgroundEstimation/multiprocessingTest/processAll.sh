aeefababbb5ac5c91bcca428450b8434e1383f8c
Marketa <marketa.jansova@iphc.cnrs.fr>
2015-11-03 17:27:22 +0100

    Adding sh for processing all macros

---
+++ backgroundEstimation/multiprocessingTest/processAll.sh
@@ -0,0 +1,13 @@
+root -l -q AddDivideMultiply.C++ ../../steerings/ROCpt.steer bsefficiency
+root -l -q AddDivideMultiply.C++ ../../steerings/ROCraw.steer bsefficiency
+root -l -q AddDivideMultiply.C++ ../../steerings/ROCpruned.steer bsefficiency
+root -l -q AddDivideMultiply.C++ ../../steerings/ROCtrimmed.steer bsefficiency
+root -l -q AddDivideMultiply.C++ ../../steerings/ROCsoftdrop.steer bsefficiency
+#root -l -q AddDivideMultiply.C++ ../../steerings/ROCmet.steer bsefficiency
+root -l -q AddDivideMultiply.C++ ../../steerings/ROCpt.steer merge
+root -l -q AddDivideMultiply.C++ ../../steerings/ROCraw.steer merge
+root -l -q AddDivideMultiply.C++ ../../steerings/ROCpruned.steer merge
+root -l -q AddDivideMultiply.C++ ../../steerings/ROCtrimmed.steer merge
+root -l -q AddDivideMultiply.C++ ../../steerings/ROCsoftdrop.steer merge
+#root -l -q AddDivideMultiply.C++ ../../steerings/ROCmet.steer merge
+root -l -q AddDivideMultiply.C++ ../../steerings/pteff.steer distrandeff
