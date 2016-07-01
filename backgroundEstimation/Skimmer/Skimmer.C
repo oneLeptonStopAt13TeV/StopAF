411121f20c02dad85b1c6da0e6aaf09a315235bf
Eric Chabert <Eric.Chabert@cern.ch>
2016-07-01 12:19:24 +0200

    generalization. Can choose inputfilename, outputfilename, treename and selection in the command line during the exectution

--- backgroundEstimation/Skimmer/Skimmer.C
+++ backgroundEstimation/Skimmer/Skimmer.C
@@ -10,26 +10,33 @@ using namespace std;
 #define DPM_DIR "FlatTree/triggerV1/SingleMuon/Run2016B_PromptReco_v2_MINIAOD/160622_212210/0000/"
 #define NUMBER_OF_FILES 499
 #define DATASET "SingleElectron"
+//#define TREE_NAME "FlatTree/tree"
+#define TREE_NAME "babyTuple"
 
-//Skimming the files for selection
-//met_pt>150 [line 28]
 
+// --------------------------------------------------------
+// name of the tree: treename
+// selection applied: selection
+// name of the file to read: ifilename
+// --------------------------------------------------------
 
-void SkimOneFile(int number){
+void SkimOneFile(string ifilename, string treename, string selection, string ofilename){
 	cout<<"@@ START @@ " <<endl;
-	cout<<"taking care of file "<<number<<endl;
-	string dpm_path = "root://sbgse1.in2p3.fr//cms/phedex/store/user/echabert/";
-	stringstream fullfilename;
-	fullfilename<<dpm_path<<DPM_DIR<<"output_"<<number<<".root";
-	cout<<"filename: "<<fullfilename.str()<<endl;
-	TFile* fin = TFile::Open(fullfilename.str().c_str());
-	TTree* tree = (TTree*) fin->Get("FlatTree/tree");
+	//cout<<"taking care of file "<<number<<endl;
+	//string dpm_path = "root://sbgse1.in2p3.fr//cms/phedex/store/user/echabert/";
+	//stringstream fullfilename;
+	//fullfilename<<dpm_path<<DPM_DIR<<"output_"<<number<<".root";
+	//fullfilename<<base_ifilename<<number<<".root";
+	//cout<<"filename: "<<fullfilename.str()<<endl;
+	cout<<"filename: "<<ifilename<<endl;
+	TFile* fin = TFile::Open(ifilename.c_str());
+	TTree* tree = (TTree*) fin->Get(treename.c_str());
 	cout<<"Oringal tree have<<"<<tree->GetEntries()<<endl;
-	TTree* selTree = tree->CopyTree("met_pt>150");	
+	TTree* selTree = tree->CopyTree(selection.c_str());	
 	cout<<"Selected tree have "<<selTree->GetEntries()<<endl;
-	stringstream ofilename;
-	ofilename<<"DATASET"<<"_"<<number<<".root";
-	TFile* output = new TFile(ofilename.str().c_str(),"RECREATE");
+	//stringstream ofilename;
+	//ofilename<<"DATASET"<<"_"<<number<<".root";
+	TFile* output = new TFile(ofilename.c_str(),"RECREATE");
 	selTree->Write();
 	output->Write();
 	output->Close();
@@ -37,20 +44,27 @@ void SkimOneFile(int number){
 }
 
 int main(int argc, char** argv){
-	if(argc!=2){
-		cerr<<"Need to give a number"<<endl;
+	if(argc<=4){
+		cerr<<"Need to provide in the following order"<<endl;
+		cerr<<" 1 - full path and name of the input file "<<endl;
+		cerr<<"2 - name of the tree to skim "<<endl;
+		cerr<<"3 - name of the output file"<<endl;
+		cerr<<"4 - selection to be applied [to not use spaces]"<<endl;
 		return -1;
 	}
-	int number = atoi(argv[1]);
-	if(number>NUMBER_OF_FILES){
-		cerr<<"This number exceed the maximum allowed !"<<endl;
-		return -1;
-	}
-	cout<<"Run on file "<<number<<endl;
-	
+	string ifilename(argv[1]);
+	string treename(argv[2]);
+	string ofilename(argv[3]);
+	string selection;
+	for(int i=4;i<argc;i++)
+		selection+=string(argv[4]);
+
+	// Needed to be able to run on dpm file
 	gSystem->Load("/usr/lib64/libdpm.so");
-	std::thread th(SkimOneFile,number);
-	th.join();
+	SkimOneFile(ifilename, treename, selection, ofilename);
+
+	//std::thread th(SkimOneFile,number);
+	//th.join();
 	/*
 	int nofloops = NUMBER_OF_FILES/nWorkers;
 	std::vector<thread> workers;
