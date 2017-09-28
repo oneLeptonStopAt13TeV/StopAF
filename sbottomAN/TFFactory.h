dd7101accc7a334734b8c20567a586d6f9d3f38b
Marketa <marketa.jansova@iphc.cnrs.fr>
2017-09-28 15:02:17 +0200

    MET extrapolation

--- sbottomAN/TFFactory.h
+++ sbottomAN/TFFactory.h
@@ -35,7 +35,7 @@ public:
 TFnames TFmem;
 string TFRegion;
 
-private:
+public:
 
 Table tabSR;
 Table tabCR;
@@ -84,6 +84,14 @@ void readFactors(string CRtype)
     MCSR = tabSR.Get(TFmem.SR,TFmem.MC);
     MCCR = tabCR.Get(TFmem.CR,TFmem.MCtot);
 
+}
+
+void addFactors(Figure dataCR2, Figure MCSR2, Figure MCCR2)
+{
+
+    dataCR += dataCR2;
+    MCSR += MCSR2; //
+    MCCR += MCCR2;
 }
 
 Figure returnTF()
@@ -166,6 +174,117 @@ void produceTFTable(string inputTabNameSR, string inputTabNameCR, string outputN
     outTab.Print(outputName + ".txt");
     outTab.PrintLatex(outputName + ".tex");
 }
+
+
+void extrapolateTFTable(string tableName, string inputTabNameSR, string inputTabNameCR, vector<string> toExtrapolate)
+{
+
+    Table TFTable(tableName+".txt");
+    vector<string> colIdEx;
+    colIdEx = TFTable.colTags;
+    colIdEx.push_back("TFextra");
+    colIdEx.push_back("TFtotal");
+
+    uint32_t TFIndex = 0;
+    uint32_t TFTotIndex = 0;
+    uint32_t bkgTypeIndex = 0;
+    uint32_t TFExtIndex = 0;
+    for(uint32_t t=0; t< colIdEx.size(); t++)
+    {
+        if(colIdEx.at(t) == "TF")
+            TFIndex=t;
+        if(colIdEx.at(t) == "TFtotal")
+            TFTotIndex=t;
+        if(colIdEx.at(t) == TFBkg)
+            bkgTypeIndex=t;
+        if(colIdEx.at(t) == "TFextra")
+            TFExtIndex=t;
+    }
+
+    //zkopirovat tabulku do nove 
+
+    vector<string> rowIdEx = TFTable.rowTags;
+
+    Table outTabExt(colIdEx, rowIdEx);
+
+    for(uint32_t j=0; j<rowIdEx.size(); j++)
+    {
+        for(uint32_t k=0; k<TFTable.colTags.size(); k++)
+        {
+            outTabExt.Set(colIdEx.at(k), rowIdEx.at(j), TFTable.Get(k,j) );
+        }
+    }
+
+    uint32_t indexToSkip = 11111;
+
+    for(uint32_t i=0; i<TFRegions.size(); i++)
+    {
+        for(uint32_t j=0; j<toExtrapolate.size(); j++)
+        {
+            if( TFRegions.at(i).find(toExtrapolate.at(j))!=std::string::npos)
+            {
+
+		TFFactory factExt1(inputTabNameSR, inputTabNameCR, TFRegions.at(i));
+		factExt1.TFmem.MC = TFBkg; //background type
+		factExt1.readFactors(controlReg);
+                Figure dataCR1NoExt = factExt1.dataCR;
+
+		TFFactory factExt2(inputTabNameSR, inputTabNameCR, TFRegions.at(i+1));
+		factExt2.TFmem.MC = TFBkg; //background type
+		factExt2.readFactors(controlReg);
+
+                cout << " region1 " << TFRegions.at(i) << " dataCR, MCSR, MCCR  " << factExt1.dataCR.value() << " " << factExt1.MCSR.value() << " " << factExt1.MCCR.value() << endl;
+                cout << " region2 " << TFRegions.at(i+1) << " dataCR, MCSR, MCCR  " << factExt2.dataCR.value() << " " << factExt2.MCSR.value() << " " << factExt2.MCCR.value() << endl;
+
+                factExt1.addFactors(factExt2.dataCR, factExt2.MCSR, factExt2.MCCR);
+               
+
+		Figure resTFExt = factExt1.returnTF();
+		Figure resEstExt = factExt1.returnEstimate();
+                Figure extrapolationReg1 =  dataCR1NoExt/factExt1.dataCR; 
+                Figure extrapolationReg2 =   factExt2.dataCR/factExt1.dataCR; 
+
+                cout << " region1 updated " << TFRegions.at(i) << " dataCR, MCSR, MCCR  " << factExt1.dataCR.value() << " " << factExt1.MCSR.value() << " " << factExt1.MCCR.value() << " TF " << resTFExt.value() << endl;
+
+                outTabExt.Set(colIdEx.at(0), rowIdEx.at(i), factExt1.dataCR ); //data always first
+                outTabExt.Set(colIdEx.at(0), rowIdEx.at(i+1), factExt1.dataCR );
+                outTabExt.Set(colIdEx.at(TFIndex), rowIdEx.at(i), resTFExt );
+                outTabExt.Set(colIdEx.at(TFIndex), rowIdEx.at(i+1), resTFExt );
+                outTabExt.Set(colIdEx.at(bkgTypeIndex), rowIdEx.at(i), resEstExt*extrapolationReg1 );
+                outTabExt.Set(colIdEx.at(bkgTypeIndex), rowIdEx.at(i+1), resEstExt*extrapolationReg2 );
+                outTabExt.Set(colIdEx.at(TFExtIndex), rowIdEx.at(i), extrapolationReg1);
+                outTabExt.Set(colIdEx.at(TFExtIndex), rowIdEx.at(i+1), extrapolationReg2);
+
+                cout << " column " << colIdEx.at(TFIndex) << " row " << rowIdEx.at(i) << " value " << resTFExt.value() << endl;
+
+                indexToSkip = i;               
+              
+
+ 
+                //nahazet to do tabulky pro i a i+1
+                // + vysledny frakce a vsechny ostatni radky
+
+            }
+        }
+        if( !(i==indexToSkip || i-1==indexToSkip) )
+        {
+            outTabExt.Set(colIdEx.at(TFExtIndex), rowIdEx.at(i), Figure(1.0,0.0)  );
+
+        }
+
+
+    }
+
+    for(uint32_t i=0; i<TFRegions.size(); i++)
+    {
+        outTabExt.Set(colIdEx.at(TFTotIndex), rowIdEx.at(i), outTabExt.Get(colIdEx.at(TFIndex),rowIdEx.at(i)) * outTabExt.Get(colIdEx.at(TFExtIndex),rowIdEx.at(i)) );
+    }
+
+    outTabExt.Print(tableName + "Ext.txt");
+    outTabExt.PrintLatex(tableName + "Ext.tex");
+}
+
+
 
 };
 
--- sbottomAN/dataAndSignal/createTFtables.cc
+++ sbottomAN/dataAndSignal/createTFtables.cc
@@ -9,10 +9,15 @@ using namespace std;
 int main()
 {
 
-    vector<string> tfreg = { "_A_250lessMETless350" , "_A_350lessMETless450" , "_A_450lessMETless600" , "_A_600lessMETlessInf" , "_B_250lessMETless450" , "_B_450lessMETless600" , "_B_600lessMETlessInf" , "_C_250lessMETless350" , "_C_350lessMETless450" , "_C_450lessMETless550" , "_C_550lessMETless650" , "_C_650lessMETlessInf" , "_D_250lessMETless350" , "_D_350lessMETless450" , "_D_450lessMETless550" , "_D_550lessMETlessInf" , "_E_250lessMETless350" , "_E_350lessMETless550" , "_E_550lessMETlessInf" , "_F_250lessMETless450" , "_F_450lessMETlessInf" , "_G_250lessMETless350" , "_G_350lessMETless450" , "_G_450lessMETless600" , "_G_600lessMETlessInf" , "_H_250lessMETless450" , "_H_450lessMETlessInf" , "_I_250lessMETless350" , "_I_350lessMETless450" , "_I_450lessMETless550" , "_I_550lessMETlessInf" ,  };
+    vector<string> tfreg = { "_A_250lessMETless350" , "_A_350lessMETless450" , "_A_450lessMETless600" , "_A_600lessMETlessInf" , "_B_250lessMETless450" , "_B_450lessMETless600" , "_B_600lessMETlessInf" , "_C_250lessMETless350" , "_C_350lessMETless450" , "_C_450lessMETless550" , "_C_550lessMETless650" , "_C_650lessMETlessInf" , "_D_250lessMETless350" , "_D_350lessMETless450" , "_D_450lessMETless550" , "_D_550lessMETlessInf" , "_E_250lessMETless350" , "_E_350lessMETless550" , "_E_550lessMETlessInf" , "_F_250lessMETless450" , "_F_450lessMETlessInf" , "_G_250lessMETless350" , "_G_350lessMETless450" , "_G_450lessMETless600" , "_G_600lessMETlessInf" , "_H_250lessMETless450" , "_H_450lessMETlessInf" , "_I_250lessMETless350" , "_I_350lessMETless450" , "_I_450lessMETless550" , "_I_550lessMETlessInf"   };
+
+
+
+    vector<string> tfregextr = { "_B_450lessMETless600" , "_E_350lessMETless550" , "_F_250lessMETless450" , "_H_250lessMETless450"  };//only first of the two
 
     TFProducer prod(tfreg, "bkgLostLepton", "CR2l");
     prod.produceTFTable("sbottomSignal.tab", "yieldsCRs.tab", "bkgLostLeptonTF");
+    prod.extrapolateTFTable("bkgLostLeptonTF", "sbottomSignal.tab", "yieldsCRs.tab", tfregextr);
     TFProducer prod2(tfreg, "bkgOneLepFromW", "CR0b");
     prod2.produceTFTable("sbottomSignal.tab", "yieldsCRs.tab" , "bkgOneLepFromWTF");
 
